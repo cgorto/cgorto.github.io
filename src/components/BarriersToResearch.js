7435487d8f64b4463f26ba6feb673191fe1f0f3a
cgorto <christian.gorton@colorado.edu>
2024-04-21 16:17:14 -0600

    minor changes

--- src/components/BarriersToResearch.js
+++ src/components/BarriersToResearch.js
@@ -6,10 +6,10 @@ function BarriersToResearch() {
         <div>
               <h2>Barriers to Research</h2>
               <p>Frank was met with backlash from various demographics. as she conducted this research. She was met with resistance from trans people in academia stating asking trans and nonbinary individuals about menstruation would be rude and dysphoric. At a conference, someone came up to her and told her that she had stolen menstruation from women. She was also ridiculed for doing the research when she is not transgender or nonbinary. </p>
-              <p>She also experienced misogyny when trying to publish.</p>
+              {/* <p>She also experienced misogyny when trying to publish.</p>
               <p>“[Her publisher] actually originally rejected my article,” she explained. “It was like a guest editing situation, and they rejected articles that were about to do with female bodies.”</p>
               <p>Her regular editor would later ask why she didn’t send in her article and tell her they would have loved to have seen it.</p>
-              <p>“I want to highlight that there is still a layered what I would call like a palimpsest there of transphobia, but like misogyny in the trans identity as well and that exists and it's hard. It's really hard.”</p>
+              <p>“I want to highlight that there is still a layered what I would call like a palimpsest there of transphobia, but like misogyny in the trans identity as well and that exists and it's hard. It's really hard.”</p> */}
         </div>
     )
 }  
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,8 +5,7 @@ function MainContent() {
         <div>
               <h2>Sustainable Menstruation - A Period Piece:</h2>
             <p>
-                Pads and tampons are products found all over the world, especially in landfills. Their single-use nature, combined with their questionable ingredients, should make those who experience menstruation think twice about what products they use. 
-            </p>
+            Plastic pollution has more than doubled in the last twenty years. There are plastics with more obvious, even easy, alternatives like straws and grocery bags. However, some plastics go unnoticed or even untalked about such as menstrual products. At any given moment, twenty-six percent of the world is menstruating. Menstrual products, like pads and tampons, are common and constantly in use.  Their single-use nature, combined with their questionable ingredients, should make those who experience menstruation think twice about what products they use.  </p>
             <p>
                 The average menstruator will use <a href='https://www.ahpma.co.uk/menstruation_facts_and_figures/'>11,000</a> period products in their lifetime. These 11,000 single-use products can sit in a landfill for <a href='https://www.webmd.com/women/what-to-know-about-disposing-of-used-tampons'>500-800 years</a>. Traditional menstruation products, such as tampons and pads, are made of plastic and therefore cannot be composted. Even cardboard tampon applicators, which may seem better, still generate waste. Despite being cardboard, municipalities do not accept them in their recycling because they have come into contact with blood. 
             </p>
@@ -24,7 +23,7 @@ function MainContent() {
             <p>“I think it's worth it to try the menstrual cups,” said Vostral, “and they work wonderfully for a lot of people–and some people they just don't.”</p>
             <p>Unfortunately, sustainable menstrual products do have barriers like cost. Reusable period underwear can cost upwards of thirty dollars a pair, depending on the brand. </p>
             <p><a href='https://thejunecup.com/collections/frontpage'>JUNE Co.</a>, however, is an example of companies doing things right.</p>
-            <p>To ensure people have access to safe, sustainable, and affordable menstrual products, they only charge production costs for their menstrual cups. They also sanitize kits. </p>
+            <p>To ensure people have access to safe, sustainable, and affordable menstrual products, they only charge production costs for their menstrual cups. They do have other products, like discs and period underwear, that they sell for profit.</p>
             <p>Access to proper sanitation can also be a barrier to sustainable period care. Menstrual cups can be messy and need to be properly sanitized. Public restrooms aren’t the best place for changing these, as they need to be dumped and cleaned periodically.</p>
             <p>
             Access to water is also important. At home, menstrual cups can be washed in boiling water. Period underwear should be rinsed and washed in cold water in the washing machine, but not everyone has access to clean water or washers to keep these products properly sanitized.</p>
